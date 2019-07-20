fc75057794cfb538c65654c7c31e60d057308325
JunHyeock Park <wnsgur6311@gmail.com>
2019-07-20 20:28:26 +0900

    add basic middleware and its code

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 import express from 'express'
+import logger from 'morgan'
+import helmet from 'helmet'
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
 const app = express()
 
 const PORT = 4000
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`)
 
 const handleHome = (req, res) => {
   res.send('Hello from home~~~')
   console.log('user comming home')
 }
 
+const handleProfile = (req, res) => {
+  res.send('in profile')
+}
+app.use(helmet())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(logger('dev'))
+
 app.get('/', handleHome)
+app.get('/profile', handleProfile)
 
 app.listen(PORT, handleListening)
+
+const handleListening = () =>
+  console.log(`Listening on: http://localhost:${PORT}`)
