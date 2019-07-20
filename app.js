c1ac08ded20eafa727be5ba21a1c7e01a43ea1d1
JunHyeock Park <wnsgur6311@gmail.com>
2019-07-20 22:50:32 +0900

    design basic MVC

---
+++ app.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import logger from 'morgan'
+import helmet from 'helmet'
+import cookieParser from 'cookie-parser'
+import bodyParser from 'body-parser'
+import globalRouter from './routers/globalRouter'
+import userRouter from './routers/userRouter'
+import videoRouter from './routers/videoRouter'
+const app = express()
+
+app.use(helmet())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(logger('dev'))
+
+app.use('/', globalRouter)
+app.use('/user', userRouter)
+app.use('/video', videoRouter)
+
+export default app
