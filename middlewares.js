7343fb46784f8a8016ef9916d923054196d81c17
JunHyeock Park <wnsgur6311@gmail.com>
2019-07-26 00:57:27 +0900

    complete basic style

--- middlewares.js
+++ middlewares.js
@@ -7,7 +7,7 @@ const localsMiddleware = (req, res, next) => {
   res.locals.siteName = 'WebTube'
   res.locals.routes = routes
   res.locals.user = {
-    isAuthenticated: false,
+    isAuthenticated: true,
     id: 1
   }
   next()
