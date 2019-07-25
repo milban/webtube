14cf3da139489e06fb368394f3772f42c661b6ba
JunHyeock Park <wnsgur6311@gmail.com>
2019-07-25 16:43:08 +0900

    complete video related

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -4,19 +4,25 @@ import Video from '../models/Video'
 // Global
 const home = async (req, res) => {
   try {
-    const videosData = await Video.find({})
+    const videosData = await Video.find({}).sort({ _id: -1 })
     res.render('home', { pageTitle: 'Home', videosData })
   } catch (error) {
     console.log(error)
-    res.render('home', { pageTitle: 'Home', videosData })
+    res.redirect(routes.home)
   }
 } // (template, added variable to template)
-const search = (req, res) => {
+const search = async (req, res) => {
   const {
     query: { term: searchingBy }
   } = req
-  console.log(searchingBy)
-  res.render('search', { pageTitle: 'Search', searchingBy, videosData })
+  let videos = []
+  try {
+    // options: i => insensitive(대소문자 구분 x)
+    videos = await Video.find({ title: { $regex: searchingBy, $options: 'i' } }).sort({ _id: -1 })
+  } catch (error) {
+    console.log(error)
+  }
+  res.render('search', { pageTitle: 'Search', searchingBy, videos })
 }
 
 // Video
@@ -37,9 +43,56 @@ const postUploadVideo = async (req, res) => {
   res.redirect(routes.videoDetail(newVideo.id))
 }
 
-const videoDetail = (req, res) => res.render('videoDetail', { pageTitle: 'VideoDetail' })
-const editVideo = (req, res) => res.render('editVideo', { pageTitle: 'EditVideo' })
-const deleteVideo = (req, res) => res.render('deleteVideo', { pageTitle: 'DeleteVideo' })
+const videoDetail = async (req, res) => {
+  const {
+    params: { id }
+  } = req
+  try {
+    const video = await Video.findById(id)
+    console.log(video)
+    res.render('videoDetail', { pageTitle: video.title, video })
+  } catch (error) {
+    console.log(error)
+    res.redirect(routes.home)
+  }
+}
+
+const getEditVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req
+  try {
+    const video = await Video.findById(id)
+    console.log(video)
+    res.render('editVideo', { pageTitle: 'Edit Video', video })
+  } catch (error) {
+    console.log(error)
+    res.redirect(routes.home)
+  }
+}
+const postEditVideo = async (req, res) => {
+  const {
+    params: { id },
+    body: { title, description }
+  } = req
+  try {
+    await Video.findByIdAndUpdate(id, { title, description })
+    res.redirect(routes.videoDetail(id))
+  } catch (error) {
+    res.redirect(routes.home)
+  }
+}
+const deleteVideo = async (req, res) => {
+  const {
+    params: { id }
+  } = req
+  try {
+    await Video.findByIdAndRemove(id)
+  } catch (error) {
+    console.log(error)
+  }
+  res.redirect(routes.home)
+}
 
 export {
   home,
@@ -48,6 +101,7 @@ export {
   getUploadVideo,
   postUploadVideo,
   videoDetail,
-  editVideo,
+  getEditVideo,
+  postEditVideo,
   deleteVideo
 }
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -3,10 +3,11 @@ import routes from '../routes'
 import {
   videos,
   videoDetail,
-  editVideo,
   deleteVideo,
   getUploadVideo,
-  postUploadVideo
+  postUploadVideo,
+  getEditVideo,
+  postEditVideo
 } from '../controllers/videoController'
 import { uploadVideo } from '../middlewares'
 
@@ -16,7 +17,8 @@ videoRouter.get(routes.home, videos)
 videoRouter.get(routes.uploadVideo, getUploadVideo)
 videoRouter.post(routes.uploadVideo, uploadVideo, postUploadVideo)
 videoRouter.get(routes.videoDetail(), videoDetail)
-videoRouter.get(routes.editVideo, editVideo)
-videoRouter.get(routes.deleteVideo, deleteVideo)
+videoRouter.get(routes.editVideo(), getEditVideo)
+videoRouter.post(routes.editVideo(), postEditVideo)
+videoRouter.get(routes.deleteVideo(), deleteVideo)
 
 export default videoRouter
